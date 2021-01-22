a6184cca0f666046066f53f090b779a092da7885
Carl Stewart <carl@stewart.se>
2021-01-22 16:06:55 +0100

    Create Car.java
    added car class

---
+++ src/Car.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+
+public abstract class  Car {
+    public String modelName;
+    public Color color;
+    public double currentSpeed;
+    public double enginePower;
+    public int nrDoors;
+
+
+    public int getNrDoors() {
+        return nrDoors;
+    }
+
+    public double getEnginePower() { return enginePower; }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    public abstract double speedFactor();
+
+    public abstract void incrementSpeed(double amount);
+    public abstract void decrementSpeed(double amount);
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+
+}
