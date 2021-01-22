c6a22dc70c22e6a551e4265fbf8db45df8f91ae8
Carl Stewart <carl@stewart.se>
2021-01-22 16:52:45 +0100

    comments added

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,21 @@
+/**
+ * @author      Niklas  Axelsson < name @ student.gu.se>
+ * @author      Carl    Stewart < name @ student.gu.se>
+ * @author      Marcus  Uppström < name @ student.gu.se>
+ * @version     0.1                 (current version number of program)
+ */
+
 import java.awt.*;
 
 public abstract class  Car {
+    /**
+     * Car model.
+     */
     public String modelName;
+
+    /**
+     * Color of car, uses Color.
+     */
     public Color color;
     public double currentSpeed;
     public double enginePower;
@@ -22,6 +36,17 @@ public abstract class  Car {
         return color;
     }
 
+    /**
+     * Sets RAL color of car                                (1)
+     * <p>
+     * Longer description. If there were any, it would be    (2)
+     * here.
+     * <p>
+     * And even more explanations to follow in consecutive
+     * paragraphs separated by HTML paragraph breaks.
+     *
+     * @param  clr Description text text text.          (3)
+     */
     public void setColor(Color clr){
         color = clr;
     }
@@ -35,6 +60,18 @@ public abstract class  Car {
         currentSpeed = 0;
     }
 
+
+    /**
+     * Return speed factor.                           (1)
+     * <p>
+     * Longer description. If there were any, it would be    (2)
+     * here.
+     * <p>
+     * And even more explanations to follow in consecutive
+     * paragraphs separated by HTML paragraph breaks.
+     *
+     * @return Returns the speed factor.
+     */
     public abstract double speedFactor();
     public abstract void incrementSpeed(double amount);
     public abstract void decrementSpeed(double amount);
