d3967ba45a14c67123adcb62e4d9cb8237d56c07
NiklasAxelsson <niklas.axelsson94@gmail.com>
2021-01-22 16:35:04 +0100

    massa grejer

--- src/Saab95.java
+++ src/Saab95.java
@@ -2,23 +2,16 @@ import java.awt.*;
 
 public class Saab95 extends Car {
 
-    public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    private boolean turboOn;
     
     public Saab95(){
-        nrDoors = 2;
-        color = Color.red;
-        enginePower = 125;
+        setNrDoors(2);
+        setColor(Color.red);
+        setEnginePower(125);
 	    turboOn = false;
-        modelName = "Saab95";
+        setName("Saab95");
         stopEngine();
     }
-
-
 
     public void setTurboOn(){
 	    turboOn = true;
@@ -31,7 +24,7 @@ public class Saab95 extends Car {
     public double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
-        return enginePower * 0.01 * turbo;
+        return getEnginePower() * 0.01 * turbo;
     }
 
     public void incrementSpeed(double amount){
@@ -41,14 +34,5 @@ public class Saab95 extends Car {
     public void decrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
     }
-    
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(amount);
-    }
 
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(amount);
-    }
 }
--- src/Volvo240.java
+++ src/Volvo240.java
@@ -2,24 +2,24 @@ import java.awt.*;
 
 public class Volvo240 extends Car{
 
-    public final static double trimFactor = 1.25;
+    private final static double trimFactor = 1.25;
     
     public Volvo240(){
-        nrDoors = 4;
-        color = Color.black;
-        enginePower = 100;
-        modelName = "Volvo240";
+        setNrDoors(4);
+        setColor(Color.black);
+        setEnginePower(100);
+        setName("Volvo240");
         stopEngine();
     }
 
     @Override
     public double speedFactor(){
-        return enginePower * 0.01 * trimFactor;
+        return getEnginePower() * 0.01 * trimFactor;
     }
 
     @Override
     public void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,getEnginePower());
     }
 
     @Override
