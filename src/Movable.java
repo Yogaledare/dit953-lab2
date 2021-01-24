3544f8b5f01c1646e5a4b28b204202c3ba62a114
Yogaledare <marcus_uppstrom@hotmail.com>
2021-01-24 17:33:06 +0100

    javadoc comments

--- src/Movable.java
+++ src/Movable.java
@@ -1,9 +1,21 @@
+/**
+ * Interface for a movable object.
+ */
 public interface Movable {
 
+    /**
+     * Moves the object.
+     */
     void move();
 
+    /**
+     * Turns the object left.
+     */
     void turnLeft();
 
+    /**
+     * Turns the object right.
+     */
     void turnRight();
 
 }
