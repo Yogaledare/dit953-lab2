5feb93b0a7b5f966fdca6c72c45f3f36e4a9a1ab
Yogaledare <marcus_uppstrom@hotmail.com>
2021-01-25 15:05:11 +0100

    tests

--- test/TestCar.java
+++ test/TestCar.java
@@ -1,74 +1,90 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import java.awt.*;
+import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 
 import static org.junit.Assert.*;
 
 public class TestCar {
+
+    List<Car> cars = new ArrayList<>();
+
     Car saab95;
 
     @Before
-    public void setupSaab() {
+    public void setup() {
+
+
+        cars.add(new Saab95());
+        cars.add(new Volvo240());
+//        cars.add(new saab95());
+
+
         saab95 = new Saab95();
     }
 
-    @Test
+/*    @Test
     public void testGetSaab95ModelName() {
-        assertEquals("Saab95", saab95.getModelName());
+        for (Car car : cars) {
+            assertEquals("Saab95", car.getModelName());
+        }
+    }*/
+
+    @Test
+    public void startAndStop() {
+        for (Car car : cars) {
+            car.startEngine();
+            car.stopEngine();
+            assertEquals(0, car.getCurrentSpeed(), 0.0);
+        }
     }
 
     @Test
-    public void startAndStopSaab95() {
-        saab95.startEngine();
-        saab95.stopEngine();
-        assertEquals(0, saab95.getCurrentSpeed(), 0.0);
+    public void driveOneStep() {
+        for (Car car : cars) {
+            car.startEngine();
+            car.gas(1.0);
+            car.move();
+            Vector2D target = new Vector2D(0.0,1.35);
+//            System.out.println(car.getPosition().getY());
+            assertEquals(car.getPosition().getY(), target.getY(), 0.0);
+        }
     }
 
     @Test
-    public void driveSaab95OneStep() {
-        saab95.startEngine();
-        saab95.gas(1.0);
-        saab95.move();
-        Vector2D target = new Vector2D(0.0,1.35);
-        System.out.println(saab95.getPosition().getY());
-        assertEquals(saab95.getPosition().getY(), target.getY(), 0.0);
-
-
+    public void turnLeftOnce() {
+        for (Car car : cars) {
+            Vector2D target = new Vector2D(-1, 0 );
+            car.turnLeft();
+            assertEquals(target.getX(), car.getDirection().getX(), 0.01);
+            assertEquals(target.getY(), car.getDirection().getY(), 0.01);
+        }
     }
 
     @Test
-    public void turnSaab95LeftOnce() {
-        Vector2D target = new Vector2D(-1, 0 );
-        saab95.turnLeft();
-
-        // against recommendations, but anyway..
-        assertEquals(target.getX(), saab95.getDirection().getX(), 0.01);
-        assertEquals(target.getY(), saab95.getDirection().getY(), 0.01);
-
-    }
-
-    @Test
-    public void turnSaab95RightOnce() {
-        Vector2D target = new Vector2D(1, 0 );
-        saab95.turnRight();
-
-        // against recommendations, but anyway..
-        assertEquals(target.getX(), saab95.getDirection().getX(), 0.01);
-        assertEquals(target.getY(), saab95.getDirection().getY(), 0.01);
+    public void turnRightOnce() {
+        for (Car car : cars) {
+            Vector2D target = new Vector2D(1, 0 );
+            car.turnRight();
+            assertEquals(target.getX(), car.getDirection().getX(), 0.01);
+            assertEquals(target.getY(), car.getDirection().getY(), 0.01);
+        }
     }
 
 
     @Test
-    public void turnSaab95RightTwice() {
-        Vector2D target = new Vector2D(0, -1 );
-        saab95.turnRight();
-        saab95.turnRight();
-
-        // against recommendations, but anyway..
-        assertEquals(target.getX(), saab95.getDirection().getX(), 0.01);
-        assertEquals(target.getY(), saab95.getDirection().getY(), 0.01);
+    public void turnRightTwice() {
+        for (Car car : cars) {
+            Vector2D target = new Vector2D(0, -1 );
+            car.turnRight();
+            car.turnRight();
+            assertEquals(target.getX(), car.getDirection().getX(), 0.01);
+            assertEquals(target.getY(), car.getDirection().getY(), 0.01);
+        }
     }
 
     @Test
@@ -91,4 +107,28 @@ public class TestCar {
         assertTrue(saab95.getCurrentSpeed() <= saab95.getEnginePower());
     }
 
+    // todo testa clamp
+
+
 }
+
+
+        /*
+        saab95.startEngine();
+        saab95.gas(1.0);
+        saab95.move();
+        Vector2D target = new Vector2D(0.0,1.35);
+        System.out.println(saab95.getPosition().getY());
+        assertEquals(saab95.getPosition().getY(), target.getY(), 0.0);
+*/
+
+
+
+        /*Vector2D target = new Vector2D(-1, 0 );
+        saab95.turnLeft();*/
+
+// against recommendations, but anyway..
+
+
+
+// against recommendations, but anyway..
