b9c8ed5802d873412fd960a8d2d2d1d614f2afea
niklas axelsson <niklas.axelsson94@gmail.com>
2021-02-02 18:26:33 +0100

    kommentarer

--- src/FIFO.java
+++ src/FIFO.java
@@ -1,3 +1,8 @@
+/**
+ * An implemented class representing a cargo hold
+ * using the first in first out rules
+ * @param <T> The type stored in the container
+ */
 public class FIFO<T extends Storable> extends Container<T> {
 
     public FIFO(int capacity, double maxWidth, double maxLength) {
