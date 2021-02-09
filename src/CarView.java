34e5758ea4f49abd42fb49c2380e650841ac7a01
Carl Stewart <carl@stewart.se>
2021-02-09 16:17:26 +0100

    actions added

--- src/CarView.java
+++ src/CarView.java
@@ -57,11 +57,12 @@ public class CarView extends JFrame{
 
 
 
-        SpinnerModel spinnerModel =
-                new SpinnerNumberModel(0, //initial value
-                        0, //min
-                        100, //max
-                        1);//step
+        SpinnerModel spinnerModel = new SpinnerNumberModel(
+                0,       //initial value
+                0,    //min
+                100,  //max
+                1      //step
+                );
         gasSpinner = new JSpinner(spinnerModel);
         gasSpinner.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
