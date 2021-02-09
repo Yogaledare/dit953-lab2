9efcacfe860d06219faa7bd80dbe74026273f8a0
Carl Stewart <carl@stewart.se>
2021-02-09 15:34:59 +0100

    minimal changes

--- src/CarController.java
+++ src/CarController.java
@@ -29,7 +29,7 @@ public class CarController {
         // Instance of this class
         CarController cc = new CarController();
 
-        cc.cars.add(new Volvo240());
+        cc.cars.add(new Volvo240(2.3, 5.0));
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
