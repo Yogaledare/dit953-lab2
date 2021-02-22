cb5b3c538d3969bea1b5d016226cbc155f018a61
Carl Stewart <carl@stewart.se>
2021-02-22 13:15:23 +0100

    added method signatures to IModel

--- src/IModel.java
+++ src/IModel.java
@@ -1,2 +1,13 @@
 public interface IModel {
+    void startEngine(Car car);
+    void stopEngine(Car car);
+    void gas(Car car);
+    void brake(Car car);
+    void raise(Car car);
+    void lower(Car car);
+    void setTurboOn(Car car);
+    void setTurboOff(Car car);
+    void subscribe(Observer observer);
+    void unsubscribe(Observer observer);
+
 }
--- src/Model.java
+++ src/Model.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+
 public class Model {
+    Timer timer = new Timer();
+
+    double boardWidth;
+    double boardHeight;
+
+    List<Car> cars = new ArrayList<>();
+    // Signal signal = new Signal();
+
+
 }
