2631bb81fa3837dce85dd07a50cb08edbd9019e7
Carl Stewart <carl@stewart.se>
2021-02-24 18:37:30 +0100

    added interface

---
+++ src/Model/IAddRemovePanel.java
@@ -0,0 +1,8 @@
+package Model;
+
+import java.awt.event.ActionListener;
+
+public interface IAddRemovePanel {
+    void setAddCarButtonActionListener(ActionListener listener);
+    void setRemoveCarButtonActionListener(ActionListener listener);
+}
