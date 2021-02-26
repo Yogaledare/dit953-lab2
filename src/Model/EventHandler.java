34210c5381cff4635baed6845ea6781840505cf5
Carl Stewart <carl@stewart.se>
2021-02-26 08:45:36 +0100

    Added JavaDoc

--- src/Model/EventHandler.java
+++ src/Model/EventHandler.java
@@ -4,22 +4,39 @@ package Model;
         import java.util.ArrayList;
 
 public class EventHandler<T extends EventObserver, I > {
-    private List<T> subscribers = new ArrayList<>();
+    private final List<T> subscribers = new ArrayList<>();
 
+    /**
+     * Adds a new subscriber to an object that is observable,
+     * for example ILoggable or IPaintable
+     * @param observer added as a listener to observable object.
+     */
     public void addSubscriber(T observer){
         subscribers.add(observer);
     }
 
+    /**
+     * Unsubscribe from an observable object.
+     * @param observer to which unsubscribe from.
+     */
     public void removeSubscriber(T observer){
         subscribers.remove(observer);
     }
 
+    /**
+     * Notifies all observers that a event occurred.
+     * @param publisher, all objects that inherits the interface
+     */
     public void publish(List<? extends I> publisher){
         for (T observer : subscribers) {
             observer.actOnPublish(publisher);
         }
     }
 
+    /**
+     * Returns the total number of subscribers.
+     * @return int total numbers.
+     */
     public int getSize() {
         return subscribers.size();
     }
--- src/View/LoggerPanel.java
+++ src/View/LoggerPanel.java
@@ -3,13 +3,15 @@ package View;
 import Model.EventHandler;
 import Model.EventObserver;
 import javax.swing.*;
+import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.List;
 
 // This panel prints car and velocity.
 // for each car a new label is created.
 public class LoggerPanel extends JPanel implements EventObserver<ILoggable> {
     JTextArea jTextArea = new JTextArea("<Car> : <speed>");
-
+    JLabel[] jLabels = new JLabel[10];
     // Initializes the panel and reads the images
     public LoggerPanel(EventHandler<EventObserver<ILoggable>,ILoggable> handler) {
         handler.addSubscriber(this);
@@ -20,11 +22,11 @@ public class LoggerPanel extends JPanel implements EventObserver<ILoggable> {
     @Override
     public void actOnPublish(List<? extends ILoggable> loggable) {
         StringBuilder res = new StringBuilder();
+        DecimalFormat df = new DecimalFormat("###.#");
 
         for (ILoggable e : loggable) {
-            res.append(e.getModelName()).append(" : ").append(e.getCurrentSpeed()).append("\n");
+            res.append(e.getModelName()).append(" : ").append(df.format(e.getCurrentSpeed())).append("\n");
         }
-
         jTextArea.setText(res.toString());
     }
 
