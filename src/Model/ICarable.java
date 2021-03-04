e80ff5a51ce487c628842216ba065312a08b3315
niklas axelsson <niklas.axelsson94@gmail.com>
2021-03-04 15:06:07 +0100

    very commit, much big

---
+++ src/Model/ICarable.java
@@ -0,0 +1,41 @@
+package Model;
+
+public interface ICarable {
+
+
+    /**
+     * If speed > 0 then its moving.
+     * @return true if currentspeed > 0
+     */
+    boolean isMoving();
+
+    ICarable move();
+    ICarable turnAround();
+    ICarable startEngine();
+    ICarable stopEngine();
+    ICarable gas(double amount);
+    ICarable brake(double amount);
+
+
+    /**
+     * Returns current position
+     */
+    Vector2D getPosition();
+
+
+    Vector2D getDirection();
+    /**
+     *
+     * @return
+     */
+    String getModelName();
+
+    /**
+     *
+     * @return
+     */
+    double getCurrentSpeed();
+
+
+
+}
