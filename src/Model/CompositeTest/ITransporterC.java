f4d53ded90dc626612c994fdabf863f73ff9324f
Carl Stewart <carl@stewart.se>
2021-03-05 12:09:13 +0100

    removed pair

--- src/Model/CompositeTest/ITransporterC.java
+++ src/Model/CompositeTest/ITransporterC.java
@@ -3,7 +3,8 @@ package Model.CompositeTest;
 import Model.ITransportable;
 import Model.RemovingMethod;
 import Model.Vector2D;
-import javafx.util.*;
+// import javafx.util.*;
+
 /**
  * Transporter interface enables an object to act as a transporter.
  * @param <T> things to transport
@@ -11,7 +12,7 @@ import javafx.util.*;
 public interface ITransporterC<T extends ITransportable> {
 
     Model.ITransporter<T> store(T item);
-    Pair<T, Model.ITransporter<T>> remove(RemovingMethod removingMethod);
+//    Pair<T, Model.ITransporter<T>> remove(RemovingMethod removingMethod);
     int getSize();
     Vector2D getPosition();
     Vector2D getDirection();
