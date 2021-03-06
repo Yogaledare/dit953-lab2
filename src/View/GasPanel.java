d17c583c430baeaf92a8fb0946ff2be5abcf7746
Carl Stewart <carl@stewart.se>
2021-03-06 10:42:06 +0100

    cleanup

--- src/View/GasPanel.java
+++ src/View/GasPanel.java
@@ -20,30 +20,22 @@ public class GasPanel extends JPanel implements IGasPanel {
                 1      //step
         );
         gasSpinner = new JSpinner(spinnerModel);
-//        Dimension d = gasSpinner.getPreferredSize();
         gasSpinner.setMinimumSize(gasSpinner.getPreferredSize());
         gasSpinner.setMaximumSize(gasSpinner.getPreferredSize());
 
 
         this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
-//        this.setLayout(new BoxLayout(new GridLayout(2,5)));
-//        this.setMinimumSize(new Dimension(minWidth, minHeight));
-//        this.setSize(minWidth, minHeight);
+
         gasSpinnerGroup.setLayout(new BoxLayout(gasSpinnerGroup, BoxLayout.PAGE_AXIS));
         gasSpinnerGroup.add(gasLabel);
         gasSpinnerGroup.add(gasSpinner);
-//        gasSpinnerGroup.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+
         this.add(gasSpinnerGroup);
         this.add(Box.createHorizontalStrut(5));
         this.add(gasButton, BorderLayout.LINE_START);
         this.add(brakeButton, BorderLayout.LINE_END);
 
     }
-
-//     typo ?
-//     public void setGasSpinnerChangeListener(ChangeListener listener) {
-//        gasSpinner.addChangeListener(listener);
-//     }
 
     @Override
     public void setSpinnerChangeListener(ChangeListener listener) {
